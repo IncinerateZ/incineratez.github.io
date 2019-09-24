880e07f9de74d73c38a0474a2e7c9b501402c034
IncinerateZ <inferious77@gmail.com>
2019-09-24 20:42:25 +0700

    .

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 var toggle = false;
 function click(e){
     var dragbox = document.getElementById("drag-box");
+    var destbox = document.getElementById("dest-box");
     boxX = parseInt(dragbox.style.left.split("px")[0]);
-    boxY = parseInt(dragbox.style.top.split("px")[0]);
-    if(e.pageX >= boxX && e.pageX <= boxX + 100 && e.pageY >= boxY && e.pageY <= boxY + 80){
+    boxY = parseInt(dragbox.style.top.split("px")[0]) + 50;
+    dX = parseInt(destbox.style.left.split("px")[0]);
+    dY = parseInt(destbox.style.top.split("px")[0]) + 50;
+    if(e.pageX >= boxX && e.pageX <= boxX + 100 && e.pageY >= boxY && e.pageY <= (boxY + 31.5)){
         toggle = !toggle;
-        console.log("TRUE");    
-    } else {
-        console.log(e.pageX + ":" + boxX);
-        console.log(e.pageY + ":" + boxY);
+        dragbox.style.height = "31.5px";
+        dragbox.style.border = "2px solid black";
+        dragbox.style.background = "white";
+        dragbox.style.color = "black";
+    }
+    if(e.pageX >= dX && e.pageX <= dX + 100 && e.pageY >= dY && e.pageY <= (dY + 31.5)){
+        dragbox.style.top = dY - 50.5 + "px";
+        dragbox.style.left = dX + "px";
+        dragbox.style.height = "33px";
+        dragbox.style.border = 0 + "px";
+        dragbox.style.background = "rgb(64, 42, 95)";
+        dragbox.style.color = "rgb(64, 42, 95)";
     }
 }
 
