0aad36cf66bc50b6e0bdbab95dbc241101b09e7a
IncinerateZ <inferious77@gmail.com>
2019-09-30 19:18:49 +0700

    .

---
+++ Projects/ReadWrite/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', load, false);
+function load(){
+    var form = document.getElementById("text-form");
+    var label = document.getElementById("label");
+    form.onsubmit = function(e){
+        e.preventDefault();
+        console.log(form.Text.value);
+        label.innerHTML = form.Text.value;
+
+        var blob = new Blob(["This is my first text."], {type: "text/plain;charset=utf-8"});
+        var file = new File(["This is my first text."], "test.txt");
+
+
+        form.reset();
+    };
+}
