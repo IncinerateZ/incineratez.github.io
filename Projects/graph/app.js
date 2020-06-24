d04b4160e1d7ee451a184bf78e29449dbbe05d6c
IncinerateZ <inferious77@gmail.com>
2020-06-24 18:27:56 +1000

    Drawing Resize

--- Projects/graph/app.js
+++ Projects/graph/app.js
@@ -25,6 +25,36 @@ function draw(canvas, points, offset) {
     ctx.stroke();
 }
 
+function distance(point, x, y) {
+    return Math.sqrt(Math.pow(x - point[0], 2) + Math.pow(y - point[1], 2));
+}
+
+function upsize(points, mX, mY) {
+    var nearest = points[0];
+    points.forEach(point => {
+        if(distance(point, mX, mY) < distance(nearest, mX, mY)) {
+            nearest = point;
+        }
+        point[0] += (point[0] - mX)/10;
+        point[1] += (point[1] - mY)/10;
+    });
+    offset[0] -= (nearest[0] - mX)/10;
+    offset[1] -= (nearest[1] - mY)/10;
+}
+
+function downsize(points, mX, mY) {
+    var nearest = points[0];
+    points.forEach(point => {
+        if(distance(point, mX, mY) < distance(nearest, mX, mY)) {
+            nearest = point;
+        }
+        point[0] -= (point[0] - mX)/10;
+        point[1] -= (point[1] - mY)/10;
+    });
+    offset[0] += (nearest[0] - mX)/10;
+    offset[1] += (nearest[1] - mY)/10;
+}
+
 window.onload = function() {
     //canvas setup
     const canvas = document.getElementById("canvas");
@@ -42,10 +72,12 @@ window.onload = function() {
     canvas.onmousedown = function(e) {
         down = true;
         mousePrev = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
+        canvas.style.cursor = "grabbing";
     }
 
     canvas.onmouseup = function(e) {
         down = false;
+        canvas.style.cursor = "grab";
     }
 
     canvas.onmousemove = function(e) {
@@ -56,5 +88,14 @@ window.onload = function() {
         //console.log(offset[0], offset[1]);
         draw(canvas, points, offset);
         mousePrev = [x, y];
+    }
+
+    canvas.onwheel = function(e) {
+        if(e.deltaY < 0) {
+            upsize(points, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        } else if(e.deltaY > 0){
+            downsize(points, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+        }
+        draw(canvas, points, offset);
     }
 }
