bc01a9927acc0b4bc24135474bd201fb121a616b
IncinerateZ <inferious77@gmail.com>
2020-07-15 21:37:12 +1000

    Client side api call test

--- Projects/graph/app.js
+++ Projects/graph/app.js
@@ -5,6 +5,26 @@ var offset = [500, 50]
 const CANVAS_SCALE = 4;
 const DRAG_ACCEL = [2.3, 1.2];
 
+function drawGrid(canvas) {
+    var sizing = [canvas.width, canvas.height];
+    var ctx = canvas.getContext("2d");
+    ctx.strokeStyle = "#b3a398";
+    //draw vertical lines
+    for(let i = 0; i <= canvas.width; i+= canvas.width/15) {
+        ctx.beginPath();
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+    }
+    //draw horizontal lines
+    for(let i = 0; i <= canvas.height; i+= canvas.height/15) {
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+        ctx.stroke();
+    }
+}
+
 function clearCanvas(canvas) {
     var ctx = canvas.getContext("2d");
     ctx.fillStyle = "#d3d3d3";
@@ -14,7 +34,8 @@ function clearCanvas(canvas) {
 function draw(canvas, points, offset) {
     var ctx = canvas.getContext("2d");
     clearCanvas(canvas);
-    ctx.fillStyle = "black";
+    drawGrid(canvas);
+    ctx.strokeStyle = "black";
     ctx.beginPath();
     let c = 0;
     points.forEach(point => {
@@ -56,6 +77,7 @@ function downsize(points, mX, mY) {
 }
 
 window.onload = function() {
+    var target = document.getElementById("isGrabbing");
     //canvas setup
     const canvas = document.getElementById("canvas");
     //console.log(canvas.width * CANVAS_SCALE + " " + canvas.height * CANVAS_SCALE);
@@ -73,11 +95,27 @@ window.onload = function() {
         down = true;
         mousePrev = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
         canvas.style.cursor = "grabbing";
+        target.innerHTML = "Is Grabbing"
     }
 
     canvas.onmouseup = function(e) {
         down = false;
         canvas.style.cursor = "grab";
+        target.innerHTML = "Not Grabbing"
+        var name = document.getElementById("name").value;
+        console.log(name);
+        const userAction = async () => {
+            const response = await fetch('https://google.incineratez.tech/hits/' + name, {
+                headers: {
+                    //'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            });
+            const res = await response.json();
+            console.log(res);
+            target.innerHTML = res["email"];
+        }
+        userAction();
     }
 
     canvas.onmousemove = function(e) {
