905232ef02696e2ff343e97f9ccca4ad34192625
IncinerateZ <inferious77@gmail.com>
2020-07-16 16:25:22 +1000

    Scroll Load New Images

--- Projects/clientrender/app.js
+++ Projects/clientrender/app.js
@@ -1,5 +1,5 @@
 const IMGBANK = "https://api.unsplash.com/photos/random?orientation=landscape&count=4";
-const IMG_MAX = 12;
+var IMG_MAX = 12;
 
 const NOIMG = '<svg id="noimg" width="312" height="245" viewBox="0 0 312 245" fill="none" xmlns="http://www.w3.org/2000/svg">' +
 '<rect x="1" y="1" width="310" height="243" rx="19" stroke="black" stroke-width="2" stroke-miterlimit="2.32452" stroke-linecap="round" stroke-linejoin="round"/>' +
@@ -12,6 +12,10 @@ var lastDownTarget;
 var lastCard;
 var count = 0;
 
+var content;
+var popupcont;
+var imgwidth;
+var imgheight;
 
 function sleep(milliseconds) {
     var start = new Date().getTime();
@@ -22,27 +26,45 @@ function sleep(milliseconds) {
     }
 }
 
+function createImg(i) {
+    content.innerHTML += "<div class='img-shadow' id='img-shadow-" + (i) + "'></div>";
+    const shadow = document.getElementById("img-shadow-" + (i));
+    shadow.style.width = imgwidth + "px";
+    shadow.style.height = imgheight + "px";
+
+    shadow.innerHTML += '<img id="' + (i) + '"></img>';
+    //shadow.innerHTML += NOIMG;
+
+    //popup card
+    popupcont.innerHTML += "<div class='popup' id='popup-" + i + "'><div class='popup-content'></div>"
+    popupcont.style.marginLeft = "-10000px";
+
+    const temp = document.getElementById("popup-" + i);
+    temp.firstChild.innerHTML = "No Image Found (" + i + ").";
+}
+
+function fillCard(res) {
+    res.forEach(pic => {
+        const img = document.getElementById(count);
+        const card = document.getElementById("popup-" + count);
+        card.firstChild.innerHTML = "Photo by <a href='" + pic["user"]["links"]["html"] +"'>" + pic["user"]["name"] + "</a> on <a href='https://unsplash.com/?utm_source=Personal_App&utm_medium=referral'>Unsplash</a>";
+        img.src = pic["urls"]["thumb"];
+
+        count++;
+    });
+}
+
 window.onload = function() {
-    const content = document.getElementById("content");
-    const popupcont = document.getElementById("popup-container");
-    const imgwidth = (content.clientWidth - 4*20)/4;
-    const imgheight = imgwidth/1.7777777778;
+    const pagebg = document.getElementById("page-bg");
+    pagebg.style.height = "675px";
+
+    content = document.getElementById("content");
+    popupcont = document.getElementById("popup-container");
+    imgwidth = (content.clientWidth - 4*25)/4;
+    imgheight = imgwidth/1.7777777778;
 
     for(let i = 0; i < IMG_MAX; i++) {
-        content.innerHTML += "<div class='img-shadow' id='img-shadow-" + (i) + "'></div>";
-        const shadow = document.getElementById("img-shadow-" + (i));
-        shadow.style.width = imgwidth + "px";
-        shadow.style.height = imgheight + "px";
-
-        shadow.innerHTML += '<img id="' + (i) + '"></img>';
-        //shadow.innerHTML += NOIMG;
-
-        //popup card
-        popupcont.innerHTML += "<div class='popup' id='popup-" + i + "'><div class='popup-content'></div>"
-        popupcont.style.marginLeft = "-10000px";
-
-        const temp = document.getElementById("popup-" + i);
-        temp.firstChild.innerHTML = "No Image Found (" + i + ").";
+        createImg(i);
     }
 
     // DEPRECATED
@@ -79,15 +101,8 @@ window.onload = function() {
         });
         const res = await response.json();
 
-        console.log(res);
-        res.forEach(pic => {
-            const img = document.getElementById(count);
-            const card = document.getElementById("popup-" + count);
-            card.firstChild.innerHTML = "Photo by <a href='" + pic["user"]["links"]["html"] +"'>" + pic["user"]["name"] + "</a> on <a href='https://unsplash.com/?utm_source=Personal_App&utm_medium=referral'>Unsplash</a>";
-            img.src = pic["urls"]["thumb"];
+        fillCard(res);
 
-            count++;
-        });
         if(count < IMG_MAX) {
             setTimeout(() => {
                 getImages()
@@ -97,8 +112,10 @@ window.onload = function() {
     getImages();
 
     document.addEventListener('mousedown', function(event) {
+        if(!(event.which === 1)) return;
         lastDownTarget = event.target;
         if(!isNaN(lastDownTarget.id)) {
+            if(lastDownTarget.id === "") return;
             popupcont.style.marginLeft = "0px";
             const card = document.getElementById("popup-" + lastDownTarget.id);
             card.style.zIndex = 10000;
@@ -110,3 +127,32 @@ window.onload = function() {
         }
     }, false);
 }
+
+window.onscroll = function(ev) {
+    const pagebg = document.getElementById("page-bg");
+    if ((window.innerHeight + window.pageYOffset) >= parseInt(pagebg.style.height)) {
+        pagebg.style.height = (parseInt(pagebg.style.height) + 2*175.5) + "px";
+        for(let i = IMG_MAX; i < IMG_MAX + 8; i++) {
+            createImg(i);
+            const getImages = async () => {
+                const response = await fetch(IMGBANK, {
+                    headers: {
+                        'Accept-Version' : 'v1',
+                        'Authorization' : 'Client-ID nANLvJnLlK9S6wo_2WI5RJ0BU5ux6smSjBsVZT8z6sY'
+                    }
+                });
+                const res = await response.json();
+        
+                fillCard(res);
+        
+                if(count < IMG_MAX + 8) {
+                    setTimeout(() => {
+                        getImages()
+                    }, 1000);
+                }
+            }
+            getImages();
+        }
+        IMG_MAX += 8;
+    }
+};
